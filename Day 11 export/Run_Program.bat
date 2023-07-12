4ff5d4cf4038d4d180c2ecd07dd977db9348b0d3
maxdgibson <93420201+maxdgibson@users.noreply.github.com>
2023-07-12 14:59:51 -0700

    Update Run_Program.bat

--- Day 11 export/Run_Program.bat
+++ Day 11 export/Run_Program.bat
@@ -1,10 +1,10 @@
 @echo off
 echo Running Script 1...
-start "" "C:\Users\mgibson\PycharmProjects\pythonpProject\venv\Scripts\python.exe" "C:\Users\mgibson\PycharmProjects\pythonProject\main.py"
+start "" "C:\Users\mgibson\PycharmProjects\pythonProject\venv\Scripts\python.exe" "C:\Users\mgibson\PycharmProjects\pythonProject\main.py"
 
 
 echo Running Script 2...
-start "" "C:\Users\mgibson\PycharmProjects\pythonpProject\venv\Scripts\python.exe"  "C:\Users\mgibson\PycharmProjects\pythonProject\ProgressCheck.py"
+start "" "C:\Users\mgibson\PycharmProjects\pythonProject\venv\Scripts\python.exe"  "C:\Users\mgibson\PycharmProjects\pythonProject\ProgressCheck.py"
 
 
 echo All scripts executed.
